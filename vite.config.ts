221e28e831012981eb650a40d18d3d40c2127fd4
linymin <43949449+linymin@users.noreply.github.com>
2025-11-30 11:38:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.DOUBAO_API_KEY),
+        'process.env.DOUBAO_API_KEY': JSON.stringify(env.DOUBAO_API_KEY)
       },
       resolve: {
         alias: {
@@ -20,4 +20,5 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
+});
 });
